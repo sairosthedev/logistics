b2c28eaddcbc5aa5f1607d7fd8c673ebbd57eab6
Kuvimbanashe <126201302+Kuvimbanashe@users.noreply.github.com>
2024-11-14 15:56:45 +0200

    mature changes

--- app/src/pages/auth/SignUp.jsx
+++ app/src/pages/auth/SignUp.jsx
@@ -88,42 +88,42 @@ function Signup() {
 
     return (
         <section className="min-h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: `url(${backgroundImage})` }}>
-            <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-2xl w-full">
-                <div className="bg-white bg-opacity-95 p-8 rounded-2xl shadow-2xl backdrop-filter backdrop-blur-lg border border-gray-200">
+            <div className="px-4 h-fit w-fit">
+                <div className="bg-white px-8  rounded-2xl shadow-2xl backdrop-filter backdrop-blur-lg border border-gray-200">
                     <div className="text-center mb-8">
-                        <img className="w-auto h-32 mx-auto mb-6 drop-shadow-md" src={mainLogo} alt="Main Logo" />
-                        <h1 className="text-4xl font-extrabold text-gray-900 mb-2">Join Truck-Stop</h1>
-                        <p className="text-xl text-gray-600">Create your account and start your journey</p>
+                        <img className="w-auto h-24 mx-auto mb-2 " src={mainLogo} alt="Main Logo" />
+                        <h1 className="text-2xl font-extrabold text-gray-900 mb-2">Join Truck-Stop</h1>
+                        <p className="text-lg text-gray-600">Create your account and start your journey</p>
                     </div>
 
-                    {errorMessage && <div className="mb-4 text-red-600 text-center">{errorMessage}</div>}
-                    {loading && <div className="mb-4 text-blue-600 text-center">Loading...</div>} {/* Loading animation */}
+                    {errorMessage && <div className="mb-2 text-red-600 text-center">{errorMessage}</div>}
+                    {loading && <div className="mb-2 text-blue-600 text-center">Loading...</div>} {/* Loading animation */}
 
-                    <form onSubmit={handleSubmit} className="space-y-6">
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                    <form onSubmit={handleSubmit} className="">
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-2">
                             <div>
                                 <label htmlFor="firstName" className="block text-sm font-medium text-gray-700 mb-1">First Name</label>
-                                <input type="text" name="firstName" id="firstName" placeholder="John" className="block w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition duration-150 ease-in-out" required aria-label="First Name" onChange={(e) => setFirstName(e.target.value)} />
+                                <input type="text" name="firstName" id="firstName" placeholder="John" className="block w-full px-4 py-1 border border-gray-300 rounded-lg shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition duration-150 ease-in-out" required aria-label="First Name" onChange={(e) => setFirstName(e.target.value)} />
                             </div>
                             <div>
                                 <label htmlFor="lastName" className="block text-sm font-medium text-gray-700 mb-1">Last Name</label>
-                                <input type="text" name="lastName" id="lastName" placeholder="Doe" className="block w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition duration-150 ease-in-out" required aria-label="Last Name" onChange={(e) => setLastName(e.target.value)} />
+                                <input type="text" name="lastName" id="lastName" placeholder="Doe" className="block w-full px-4 py-1 border border-gray-300 rounded-lg shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition duration-150 ease-in-out" required aria-label="Last Name" onChange={(e) => setLastName(e.target.value)} />
                             </div>
                             <div className="md:col-span-2">
                                 <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">Email Address</label>
-                                <input type="email" name="email" id="email" placeholder="you@example.com" className="block w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition duration-150 ease-in-out" required aria-label="Email Address" onChange={(e) => setEmail(e.target.value)} />
+                                <input type="email" name="email" id="email" placeholder="you@example.com" className="block w-full px-4 py-1 border border-gray-300 rounded-lg shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition duration-150 ease-in-out" required aria-label="Email Address" onChange={(e) => setEmail(e.target.value)} />
                             </div>
                             <div>
                                 <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" className="block w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition duration-150 ease-in-out" required aria-label="Password" onChange={(e) => setPassword(e.target.value)} />
+                                <input type="password" name="password" id="password" placeholder="••••••••" className="block w-full px-4 py-1 border border-gray-300 rounded-lg shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition duration-150 ease-in-out" required aria-label="Password" onChange={(e) => setPassword(e.target.value)} />
                             </div>
                             <div>
                                 <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700 mb-1">Confirm Password</label>
-                                <input type="password" name="confirmPassword" id="confirmPassword" placeholder="••••••••" className="block w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition duration-150 ease-in-out" required aria-label="Confirm Password" onChange={(e) => setConfirmPassword(e.target.value)} />
+                                <input type="password" name="confirmPassword" id="confirmPassword" placeholder="••••••••" className="block w-full px-4 py-1 border border-gray-300 rounded-lg shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition duration-150 ease-in-out" required aria-label="Confirm Password" onChange={(e) => setConfirmPassword(e.target.value)} />
                             </div>
                             <div className="md:col-span-2">
                                 <label htmlFor="account-type" className="block text-sm font-medium text-gray-700 mb-1">Account Type</label>
-                                <select id="account-type" name="account-type" className="block w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition duration-150 ease-in-out" onChange={handleAccountTypeChange} value={accountType} required aria-label="Account Type">
+                                <select id="account-type" name="account-type" className="block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition duration-150 ease-in-out" onChange={handleAccountTypeChange} value={accountType} required aria-label="Account Type">
                                     <option value="" disabled hidden>Select Role</option>
                                     <option value="trucker">Trucker</option>
                                     <option value="client">Client</option>
@@ -132,7 +132,7 @@ function Signup() {
                             </div>
                         </div>
 
-                        <div className="flex items-center">
+                        <div className="flex items-center mt-2">
                             <input type="checkbox" name="terms" id="terms" className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" required onChange={handleTermsChange} />
                             <label htmlFor="terms" className="ml-2 block text-sm text-gray-700">
                                 I agree to the <a href="#" className="text-indigo-600 hover:text-indigo-500 font-medium">Terms and Conditions</a>
@@ -141,14 +141,14 @@ function Signup() {
 
                         <button
                             type="submit"
-                            className="w-full flex justify-center py-3 px-4 border border-transparent rounded-lg shadow-sm text-lg font-medium text-white bg-gradient-to-r from-indigo-600 to-purple-600 hover:from-indigo-700 hover:to-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-150 ease-in-out transform hover:scale-105"
+                            className="w-full flex justify-center py-1 px-4 mt-2 border border-transparent rounded-lg shadow-sm text-lg font-medium text-white bg-gradient-to-r from-indigo-600 to-purple-600 hover:from-indigo-700 hover:to-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-150 ease-in-out transform hover:scale-105"
                             disabled={loading} // Disable button while loading
                         >
                             {loading ? 'Creating Account...' : 'Create Account'} {/* Change button text based on loading state */}
                         </button>
                     </form>
 
-                    <div className="mt-8 text-center">
+                    <div className="mt-1 text-center mb-3">
                         <p className="text-sm text-gray-600">
                             Already have an account?{' '}
                             <Link to="/" className="font-medium text-indigo-600 hover:text-indigo-500 transition duration-150 ease-in-out">
--- app/src/pages/auth/login.jsx
+++ app/src/pages/auth/login.jsx
@@ -69,15 +69,16 @@ function login() {
 
     return (
         <section className="min-h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: `url(${backgroundImage})` }}>
-            <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-xl w-full">
-                <div className="bg-white p-8 rounded-xl shadow-2xl">
+            <div className="  w-fit  h-fit">
+                <div className='w-[70vh]'></div>
+                <div className="bg-white p-2 rounded-xl shadow-2xl">
                     <div className="text-center">
-                        <img className="w-auto h-24 mx-auto mb-4" src={mainLogo} alt="Main Logo" />
-                        <h1 className="text-4xl font-bold text-gray-900 mb-2">Welcome to Truck-Stop</h1>
-                        <p className="text-gray-600 mb-8">Sign in to your account</p>
+                        <img className="w-auto h-24 mx-auto mb-1" src={mainLogo} alt="Main Logo" />
+                        <h2 className="text-2xl font-bold text-gray-900 mb-2">Welcome to Truck-Stop</h2>
+                        <p className="text-gray-600 mb-3">Sign in to your account</p>
                     </div>
 
-                    <div className="space-y-6">
+                    <div className="space-y-3">
                         {/* 
                         <button
                             type="button"
@@ -98,20 +99,20 @@ function login() {
                         </div> */}
 
                         <form onSubmit={handleSubmit}>
-                            <div className="space-y-4">
+                            <div className="space-y-3 p-5">
                                 <div>
                                     <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email Address</label>
-                                    <input type="email" name="email" id="email" placeholder="you@example.com" className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required aria-label="Email Address" onChange={(e) => setEmail(e.target.value)} />
+                                    <input type="email" name="email" id="email" placeholder="you@example.com" className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required aria-label="Email Address" onChange={(e) => setEmail(e.target.value)} />
                                 </div>
 
                                 <div>
                                     <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••" className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required aria-label="Password" onChange={(e) => setPassword(e.target.value)} />
+                                    <input type="password" name="password" id="password" placeholder="••••••••" className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required aria-label="Password" onChange={(e) => setPassword(e.target.value)} />
                                 </div>
 
                                 <div>
                                     <label htmlFor="account-type" className="block text-sm font-medium text-gray-700">Account Type</label>
-                                    <select id="account-type" name="account-type" className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" onChange={handleAccountTypeChange} required aria-label="Account Type">
+                                    <select id="account-type" name="account-type" className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" onChange={handleAccountTypeChange} required aria-label="Account Type">
                                         <option value="" disabled selected hidden>Select Role</option>
                                         <option value="admin">Admin</option>
                                         <option value="trucker">Trucker</option>
@@ -130,7 +131,7 @@ function login() {
 
                                 <button
                                     type="submit"
-                                    className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                 >
                                     {loading ? <ClipLoader size={20} color={"#fff"} /> : 'Sign in'}
                                 </button>
@@ -138,7 +139,7 @@ function login() {
                         </form>
                     </div>
 
-                    <div className="mt-6 text-center">
+                    <div className="mb-2 text-center">
                         <p className="text-sm text-gray-600">
                             Don't have an account?{' '}
                             <Link to="/signup" className="font-medium text-indigo-600 hover:text-indigo-500">
